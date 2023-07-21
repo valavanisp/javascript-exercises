eb65ad6f811af1755f837f16d53c4ddbdda244db
Panos <22086296+valavanisp@users.noreply.github.com>
2023-07-21 14:41:53 -0400

    did 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, numTimes) {
+    let newStr = '';
 
+    if (numTimes < 0)
+    {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < numTimes; i++)
+    {
+        newStr += str;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
