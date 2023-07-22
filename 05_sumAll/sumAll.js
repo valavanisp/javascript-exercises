8ae5552a60709dc33db7c809b392587dbc748d73
Panos <22086296+valavanisp@users.noreply.github.com>
2023-07-22 15:13:43 -0400

    did 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let start = 0;
+    let end = 0;
+    let sum = 0;
+
+    if (a < 0 || b < 0 || typeof a  != 'number' || typeof b  != 'number')
+    {
+        return 'ERROR';
+    }
+    if (a > b)
+    {
+        start = b;
+        end = a;
+    }
+    else
+    {
+        start = a;
+        end = b;
+    }
+
+    for (let i  = start; i <= end; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
