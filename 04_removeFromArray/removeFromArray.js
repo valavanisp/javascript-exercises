36334ff77843cf6428fafdafc2003f8b31f8731f
Panos <22086296+valavanisp@users.noreply.github.com>
2023-07-22 11:10:51 -0400

    did 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    newArr = [];
 
+    arr.forEach(element => {
+        if (!args.includes(element))
+        {
+            newArr.push(element);
+        }
+    });
+    return newArr;
+
+    //return arr.filter(x => !args.includes(x));
 };
 
 // Do not edit below this line
