0c3db198c61f0269346efe1dad8a6f2cdc291cdb
Panos <22086296+valavanisp@users.noreply.github.com>
2023-08-29 21:45:46 -0400

    did 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let s = 0;
+  for (let i = 0; i < arr[i]; i++) {
+    s += arr[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let s = 1;
+  for (let i = 0; i < arr[i]; i++) {
+    s *= arr[i];
+  }
+  return s;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let f = 1;
+  for (let i = a; i > 0; i--) {
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
