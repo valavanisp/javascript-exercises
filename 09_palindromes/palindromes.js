315c7ed2455d6f9ec74dcb0152bbeb865a1fb3d0
Panos <22086296+valavanisp@users.noreply.github.com>
2023-10-09 15:30:30 -0600

    did 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let newString =  processedString.split("").reverse().join("");
 
+    return newString === processedString;
 };
 
 // Do not edit below this line
