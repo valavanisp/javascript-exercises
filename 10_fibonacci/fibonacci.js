b2ee4dd01960097d51aa258b47c7e861c822c915
Panos <22086296+valavanisp@users.noreply.github.com>
2023-10-09 15:38:22 -0600

    did 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
 
+    let arr = new Array(num);
+
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[num];
 };
 
 // Do not edit below this line
